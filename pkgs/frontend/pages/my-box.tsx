69d2762c2f6c8aa91a139859e44a272c7887e4f1
somasekimoto <0421.soma@gmail.com>
2024-09-02 21:31:11 +0900

    Add dummy getNodeData for testing

--- pkgs/frontend/pages/my-box.tsx
+++ pkgs/frontend/pages/my-box.tsx
@@ -69,39 +69,10 @@ export default function MyBox() {
     currentNodeKey,
     setCurrentNodeKey,
   } = globalContext;
-  // const { data: getNodeData, error: getNodeError } = useGetNode(
-  //   currentNodeKey!,
-  //   currentNodeCid!
-  // );
-
-  const getNodeData = {
-    metadata: {
-      children: [
-        {
-          metadata: {
-            name: "test",
-            owner_id: "0x12345678901234567890",
-            created_at: "2024-01-01 00:00:00",
-            children: [],
-          },
-          file_data: "test",
-          cid: "QmZ4tDuvesekSs4qM5ZBKpXiZGun7S2CYtEZRB3DYXkjGx",
-        },
-      ],
-    },
-    children: [
-      {
-        metadata: {
-          name: "test",
-          owner_id: "0x12345678901234567890",
-          created_at: "2024-01-01 00:00:00",
-          children: [],
-        },
-        file_data: "test",
-        cid: "QmZ4tDuvesekSs4qM5ZBKpXiZGun7S2CYtEZRB3DYXkjGx",
-      },
-    ],
-  };
+  const { data: getNodeData, error: getNodeError } = useGetNode(
+    currentNodeKey!,
+    currentNodeCid!
+  );
 
   const [breadcrumbItems, setBreadcrumbItems] = useState<any[]>([
     {
