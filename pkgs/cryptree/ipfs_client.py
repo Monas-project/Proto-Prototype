ef428c04eb9534929db76c4423b83e7c03b30066
somasekimoto <0421.soma@gmail.com>
2024-09-14 14:26:25 +0900

    Add custom authorization token to IPFS requests

--- pkgs/cryptree/ipfs_client.py
+++ pkgs/cryptree/ipfs_client.py
@@ -1,15 +1,20 @@
 import requests
 from io import BytesIO
+import socket
 
 class IpfsClient:
-    def __init__(self, ipfs_daemon_url='http://127.0.0.1:5001'):
+    def __init__(self, ipfs_daemon_url='http://127.0.0.1:5001', token=None):
         self.ipfs_daemon_url = ipfs_daemon_url
+        self.token = token
 
     def add_bytes(self, string_data: bytes):
         url = f'{self.ipfs_daemon_url}/api/v0/add'
         string_bytes = BytesIO(string_data)
         files = {'file': ('string_data.txt', string_bytes)}
-        response = requests.post(url, files=files)
+        headers = {
+            "Authorization": f'Custom {self.token}'
+        }
+        response = requests.post(url, files=files, headers=headers)
         if response.ok:
             ipfs_hash = response.json()['Hash']
             print(f'String uploaded to IPFS with hash: {ipfs_hash}')
@@ -21,13 +26,11 @@ class IpfsClient:
 
     def cat(self, ipfs_hash):
         url = f'{self.ipfs_daemon_url}/api/v0/cat?arg={ipfs_hash}'
-        print(f'Fetching data from IPFS with hash: {ipfs_hash}')
-        print(f'URL: {url}')
-        response = requests.post(url, stream=True)
+        headers = {
+            "Authorization": f'Custom {self.token}'
+        }
+        response = requests.post(url, stream=True, headers=headers)
         if response.status_code == 200:
-            data = response.content
-            return data
+            return response.content
         else:
-            print('Error getting data from IPFS')
-            print(response.text)
-            return None
+            raise Exception(f'Error retrieving IPFS hash {ipfs_hash} status code: {response.status_code}')
